94ee5695d9d8a4688bf3771c9c1c17a54c333ba2
Englc <clemens.engl@msg.group>
2019-05-02 13:40:42 +0200

    repository

--- src/main/java/de/englc/helloWorld/TextDomain.java
+++ src/main/java/de/englc/helloWorld/TextDomain.java
@@ -8,4 +8,23 @@ public class TextDomain {
     public String get() {
         return "This is some text";
     }
+
+
+
+	/*
+
+@Repository
+public interface DomainRepository extends JpaRepository<Domain, String> {
+    Optional<Domain> findById(Long id);
+    Optional<Domain> findByIdNot(Long id);
+
+    List<Domain> findByIdIn(List<Long> id);
+    List<Domain> findByIdAndTextIn(Long id, String text);
+
+
+
+}
+
+
+	 */
 }
